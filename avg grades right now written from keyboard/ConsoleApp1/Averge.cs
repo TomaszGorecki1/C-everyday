64d5696795718fbc97f4983cb8b412d3c758ff47
Tomasz <tomigunpl@icloud.com>
2022-01-11 20:12:11 +0100

    Program count averge and highest grade with special class

---
+++ avg grades right now written from keyboard/ConsoleApp1/Averge.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Averge
+    {
+        public int[] grade= new int [100], weight =new int[100];
+        public double sum = 0, sumtmp = 0, avg = 0;
+        public int sumWeight = 0, howmanygrades=0, 
+        highestGRade=0, highestGradetmp = 0;
+
+        public void inputGradeNumber()
+        {
+            Console.Write("Enter a number of grades you have got:");
+            howmanygrades = Convert.ToInt32(Console.ReadLine());
+            if(howmanygrades < 1)
+            {
+                Console.WriteLine("Bad data, input must be bigger than 1");
+                return;
+            }
+            Console.WriteLine($" You will have to write {howmanygrades} grades you've got: ");
+        }
+        public void AvgOverall()
+        {
+            for (int i = 0; i < howmanygrades; i++)
+            {
+                Console.Write("Enter grade you have got:");
+                grade[i] = Convert.ToInt32(Console.ReadLine());
+                if (grade[i] < 1 || grade[i] > 6)
+                {
+                    Console.WriteLine("Bad data, input must be bigger than 1 and lower than 6");
+                    return;
+                }
+                else
+                {
+                    Console.Write("Enter weight of this grade:");
+                    weight[i] = Convert.ToInt32(Console.ReadLine());
+                    if (weight[i] < 1 || grade[i] > 12)
+                    {
+                        Console.WriteLine("Bad data, input must be bigger than 1 and lower than 12");
+                        return;
+                    }
+                    else
+                    {
+                        sumWeight = sumWeight + weight[i];
+                        sumtmp = grade[i] * weight[i];
+                        sum = sum + sumtmp;
+                    }
+                }
+            }
+            avg = sum / sumWeight;
+        }
+
+        public void showResults()
+        {
+            Console.WriteLine($"Your average grade is  : {avg}");
+            Console.WriteLine($"Your highest grade is: {highestGRade}");
+        }
+        public void highestGrade()
+        {
+            for (int i = 0; i < howmanygrades; i++)
+            {
+                if (grade[i]>highestGradetmp) 
+                {
+                    highestGRade = grade[i];
+                    highestGradetmp = grade[i];
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ avg grades right now written from keyboard/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Averge a1 = new Averge();
+            a1.inputGradeNumber();
+            a1.AvgOverall();
+            a1.highestGrade();
+            a1.showResults();
+        }
+    }
+
+}
+/*
+            int[] grade = new int[100];
+            int[] weight = new int[100];
+            double sumtmp = 0;
+            double sum = 0;
+            int sumWeight = 0;
+            double avg = 0;
+            int howmanygrades;
+            Console.Write("Enter a number of grades you have got:");
+            howmanygrades = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($" You will have to write {howmanygrades} grades you've got: ");
+            for (int i = 0; i < howmanygrades; i++)
+            {
+                Console.Write("Enter grade you have got:");
+                grade[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter weight of this grade:");
+                weight[i] = Convert.ToInt32(Console.ReadLine());
+                sumWeight = sumWeight + weight[i];
+                sumtmp = grade[i] * weight[i];
+                sum = sum + sumtmp;
+            }
+            avg = sum / sumWeight;
+            Console.WriteLine($"Your average grade is  : {avg}");
+            */
