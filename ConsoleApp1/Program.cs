f64b1c974b251135dbd7ca2014a6999d6d9987be
Tomasz <tomigunpl@icloud.com>
2022-03-05 22:21:22 +0100

    g

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Podaj liczbę czarnuchu ");
+                var number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Co robimy czarnuchu? ");
+                var operation = Console.ReadLine();
+                Console.WriteLine("Podaj 2 liczbę czarnuchu ");
+                var number2 = int.Parse(Console.ReadLine());
+
+                var result = 0;
+                switch (operation)
+                {
+
+                    case "+":
+                        result = number1 + number2;
+                        break;
+                    case "-":
+                        result = number1 - number2;
+                        break;
+
+                    default:
+                        throw new Exception("Nieprawidlowa operacja czarnuchu! ");
+                        break;
+                }
+                Console.WriteLine("wynik to " + result + " Czarnuchu!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+
+            
+
+            
+        }
+    }
+} 
