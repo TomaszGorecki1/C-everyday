81b30aacfef0b26adaecc3e312fb30dc2737bc9f
Tomasz <tomigunpl@icloud.com>
2022-03-17 21:38:57 +0100

    Stuff with data bases

--- xamarin/Xam_TravelRecordApp-main/TravelRecordApp/HistoryPage.xaml.cs
+++ xamarin/Xam_TravelRecordApp-main/TravelRecordApp/HistoryPage.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
-
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
@@ -10,6 +11,27 @@ namespace TravelRecordApp
         public HistoryPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            { 
+                conn.CreateTable<Post>();
+                var posts = conn.Table<Post>().ToList();
+                postListView.ItemsSource = posts;  
+            }
+        }
+
+        void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedPost = postListView.SelectedItem as Post;
+            if(selectedPost != null)
+            {
+                Navigation.PushAsync(new PostDetail(selectedPost));
+            }
         }
     }
 }
--- xamarin/Xam_TravelRecordApp-main/TravelRecordApp/NewTravelPage.xaml.cs
+++ xamarin/Xam_TravelRecordApp-main/TravelRecordApp/NewTravelPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelRecordApp.Model;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +17,32 @@ namespace TravelRecordApp
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                Experience = experienceEntry.Text
+            };
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            { 
+                conn.CreateTable<Post>();
+            int rows = conn.Insert(post);
+
+                if (rows > 0)
+                {
+                    DisplayAlert("Success", "Experience succesfully inserted", "Ok");
+                }
+                else
+                {
+                    DisplayAlert("Failure", "Experience failed to be inserted", "Ok");
+                }
+            }
+
+            
+
         }
     }
 }
